b0e3b9daf3bc64f65514d276d1dd07ed85047f59
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2022-04-25 19:38:51 +0200

    theme manager

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ColorManager{
   static Color primary = const Color(0xff667EEA);
   static Color primary2 = const Color(0xff64B6FF);
+  static Color deepBlue = const Color(0xff374ABE);
   static Color rose = const Color(0xffFF5858);
   static Color rose2 = const Color(0xffFB5895);
   static Color green = const Color(0xff43E97B);
@@ -14,8 +15,12 @@ class ColorManager{
   static Color grey = const Color(0xffA6A6A6);
   static Color grey1 = const Color(0xffA3A3A3);
   static Color grey2 = const Color(0xff707070);
-  static Color grey3 = const Color(0xff656565);
-  static Color onBlack = const Color(0xff323232);
+  static Color grey3 = const Color(0xff696969);
+  static Color grey4 = const Color(0xff656565);
+  static Color grey5 = const Color(0xff454242);
+  static Color grey6 = const Color(0xff404040);
+  static Color onBlack = const Color(0xff2A2A2A);
+  static Color onBlack1 = const Color(0xff323232);
   static Color onBlack2 = const Color(0xff3C3C3C);
   static Color onBlack3 = const Color(0xff434343);
 
--- lib/presentation/resources/font_manager.dart
+++ lib/presentation/resources/font_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FontConstants{
-  static const String fontFamily = "Montserrat";
+  static const String fontFamily = "Segoe Ui";
 }
 class FontWeightManager{
   static FontWeight light = FontWeight.w300;
@@ -11,11 +11,16 @@ class FontWeightManager{
   static FontWeight bold = FontWeight.w700;
 }
 class FontSize{
+  static const double s10 = 10.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
+  static const double s15 = 15.0;
   static const double s16 = 16.0;
   static const double s17 = 17.0;
   static const double s18 = 18.0;
   static const double s20 = 20.0;
   static const double s22 = 22.0;
+  static const double s24 = 24.0;
+  static const double s25 = 25.0;
+  static const double s30 = 30.0;
 }
