183a2c1b92be42ad5d5213146a42d6c583ed6a06
MahmoudFathiAhmed <zanoon.dx@gmail.com>
2022-04-25 16:37:37 +0200

    color manager1

--- lib/presentation/resources/color_manager.dart
+++ lib/presentation/resources/color_manager.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ColorManager{
-  static Color primary = const Color(0xffED9728);
+  static Color primary = const Color(0xff667EEA);
+  static Color primary2 = const Color(0xff64B6FF);
+  static Color rose = const Color(0xffFF5858);
+  static Color rose2 = const Color(0xffFB5895);
+  static Color green = const Color(0xff43E97B);
+  static Color green2 = const Color(0xff38F9D7);
+  static Color purple = const Color(0xff9643E9);
+  static Color purple2 = const Color(0xffBA38F9);
+  static Color offWhite = const Color(0xffE1E1E1);
+  static Color offWhite1 = const Color(0xffF3F3F3);
+
+
+
+
+  // static Color primary = const Color(0xffED9728);
   static Color darkGrey = const Color(0xff525252);
   static Color grey = const Color(0xff737477);
   static Color lightGrey = const Color(0xff9E9E9E);
   static Color black = const Color(0xff000000);
+
 
   //new colors
   static Color darkPrimary = const Color(0xffd17d11);
